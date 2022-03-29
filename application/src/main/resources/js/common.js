2669f9aaf326f652861e3aa8e3adbc60e729515f
Marko Vuckovic <marko.vuckovic@primeholding.com>
2022-03-29 16:41:28 +0200

    refactor: 💡 Refactor

--- application/src/main/resources/js/common.js
+++ application/src/main/resources/js/common.js
@@ -1,12 +1,13 @@
 const darkMode = localStorage.getItem('dark-mode');
 
-if (darkMode === 'false') {
-    u('html').addClass('light-mode');
-    u('#logo-image').attr('src', '/images/logo/affiliora-dark.png');
-    u('.switch span:last-child').html('Light Mode');
-}
-
-u('.switch input').attr('checked', darkMode === 'true');
+window.addEventListener('load', () => {
+     if (darkMode === 'false') {
+         u('html').addClass('light-mode');
+         u('#logo-image').attr('src', '/images/logo/affiliora-dark.png');
+         u('.switch span:last-child').html('Light Mode');
+     }
+     u('.switch input').attr('checked', darkMode === 'true');
+});
 
 u('.switch input').on('input', ({ target }) => {
     const checked = u(target).is(':checked');
