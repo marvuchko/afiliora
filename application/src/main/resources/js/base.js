1b3ba372df2579196dcec0d0d09b02cb2ac9c7ac
Marko Vuckovic <marko.vuckovic@primeholding.com>
2022-04-05 15:15:47 +0200

    refactor: 💡 Refactoring

--- application/src/main/resources/js/base.js
+++ application/src/main/resources/js/base.js
@@ -1,8 +1,3 @@
-window.addEventListener('unload', () => {
-    u('body').addClass("blur-sm");
-});
-
 window.addEventListener('load', () => {
      u('body').removeClass("blur-sm");
-     AOS.init();
 });
