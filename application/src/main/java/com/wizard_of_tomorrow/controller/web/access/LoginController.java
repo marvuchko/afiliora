1f198b384ac36c526ec5711530cb134560397a12
Marko Vuckovic <marko.vuckovic@primeholding.com>
2022-04-15 15:54:37 +0200

    refactor: 💡 Refactor

--- application/src/main/java/com/wizard_of_tomorrow/controller/web/access/LoginController.java
+++ application/src/main/java/com/wizard_of_tomorrow/controller/web/access/LoginController.java
@@ -1,11 +1,9 @@
 package com.wizard_of_tomorrow.controller.web.access;
 
 import com.wizard_of_tomorrow.controller.AbstractViewController;
-import com.wizard_of_tomorrow.controller.web.affiliate.DashboardController;
 import java.util.concurrent.CompletableFuture;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
